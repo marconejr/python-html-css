dc09504f328ac3678a447eba9c66dd3dd52d3973
marconejr <marconejr@yahoo.com.br>
2021-08-27 00:11:02 +0000

    Aula 2 Python

--- app.py
+++ app.py
@@ -1,32 +1,53 @@
-import sqlite3  # importando o SQLite
-from flask import Flask, request, session, g, redirect, abort, render_template, flash #importanto módulos
-
-# CONFIGURAÇÃO
-DATABASE = "blog.db" #banco
-SECRET_KEY = 'pudim' #senha
-
-app = Flask(__name__) # variavel do python que guarda o nome do arquivo a ser executado, só essa linha ela cria uma aplicação mínima
+import sqlite3
+from flask import Flask, request, url_for, session, g, redirect, abort, render_template, flash
+# configuração
+DATABASE = "blog.db"
+SECRET_KEY = 'pudim'
+app = Flask(__name__)
 app.config.from_object(__name__)
-
 def conectar_bd():
     return sqlite3.connect(app.config['DATABASE'])
-
 @app.before_request
 def antes_requisicao():
-    g.bd=conectar_bd()
-
+    g.bd = conectar_bd()
 @app.teardown_request
 def depois_request(exc):
     g.bd.close()
-
 @app.route('/')
+@app.route('/entradas')
 def exibir_entradas():
-    return render_template('exibir_entradas.html')
+    sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
+    cur = g.bd.execute(sql)
+    entradas = []
+    for titulo, texto in cur.fetchall():
+        entradas.append({'titulo': titulo, 'texto': texto})
+    return render_template('exibir_entradas.html', entradas=entradas)
 
+@app.route('/inserir')
+def inserir_entrada():
+    sql = "INSERT INTO entradas(titulo, texto) VALUES ('Terceiro Post','Esse é post 3')"
+    g.bd.execute(sql)
+    if not session.get('logado'):
+        abort(401)
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?,?)"
+    g.bd.execute(sql, request.form['campoTitulo'], request.form['campoTexto'])
+    g.bd.commit()
+    return redirect(url_for('exibir_entradas')) 
 
-@app.route('/hello') # é  decorators, onde é passado string que será uma URL, faz um link entre a url e extensão
-def pagina_inicial():
-    return "Hello World"
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)     #  .pop desloga o usuario
+    return redirect(url_for('exibir_entradas'))
 
+@app.route('/login', methods=["GET","POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['campoUsuario'] != 'admin' \
+        or request.form['campoSenha'] != 'admin'
+            erro = 'Senha ou usuário Inválidos'
+        else:
+            session['logado'] = True  
+            return render_template('exibir_entradas')
 
-
+    return render_template('login.html', erro=erro)
